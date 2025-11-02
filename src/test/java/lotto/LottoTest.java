99d7675147779fb3e96d9b475f0ed9ab4d28f3e1
zeoueon <zeoueon129@gmail.com>
2025-11-02 14:12:40 +0900

    test(LottoGenerator): 로또 생성기가 올바르게 동작하는 지 확인하는 테스트 구현
    로또 생성기가 중복이 없는 6개의 숫자를 오름 차순으로 생성하는 지 확인하는 테스트를 구현함.

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,13 +1,17 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import java.util.List;
+import lotto.domain.Lotto;
+import lotto.domain.LottoGenerator;
+import lotto.domain.RandomNumberGenerator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 class LottoTest {
+
     @Test
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
@@ -22,4 +26,24 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @Test
+    @DisplayName("로또를 생성하면 중복이 없는 6개의 숫자가 오름차순으로 생성된다.")
+    void lottoGeneratorTest() {
+        //given
+        LottoGenerator generator = new LottoGenerator(RandomNumberGenerator::getUniqueSixNumbers);
+        int lottoAmount = 3;
+
+        //when
+        List<Lotto> lottos = generator.createLottos(lottoAmount);
+
+        //then
+        assertThat(lottos).hasSize(lottoAmount);
+        assertThat(lottos).allSatisfy(lotto -> {
+            List<Integer> numbers = lotto.getNumbers();
+            assertThat(numbers).hasSize(6);
+            assertThat(numbers).isSorted();
+            assertThat(numbers).doesNotHaveDuplicates();
+        });
+    }
 }
