0f731048b99d3bb924453092f76e784e6065591f
zeoueon <zeoueon129@gmail.com>
2025-11-02 14:34:17 +0900

    feat(OutputView): 컨트롤러를 통해 전달받은 정보를 요구하는 형식으로 출력하는 객체 구현
    구매한 로또들의 번호를 출력하는 함수를 구현함.
    로또 게임의 결과를 순위 별로 출력하는 함수를 구현함.
    총 수익률을 소수점 둘째자리에서 반올림하여 출력하는 함수를 구현함.

---
+++ src/main/java/lotto/view/OutputView.java
@@ -0,0 +1,59 @@
+package lotto.view;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lotto.domain.LottoRanking;
+import lotto.dto.LottoDto;
+
+public class OutputView {
+
+    public static void printLottos(List<LottoDto> lottos) {
+        System.out.println(lottos.size() + "개를 구매했습니다.");
+
+        for (LottoDto lotto : lottos) {
+            System.out.println(lotto.getNumbers());
+        }
+    }
+
+    public static void printResults(List<LottoRanking> results) {
+        Map<LottoRanking, Integer> resultsMap = initializeResultsMap();
+        putResults(results, resultsMap);
+
+        for (Map.Entry<LottoRanking, Integer> entry : resultsMap.entrySet()) {
+            System.out.println(entry.getKey().getStringFormat() + entry.getValue() + "개");
+        }
+    }
+
+    public static void printProfitRate(double profitRate) {
+        double roundProfitRate = Math.round(profitRate * 100) / 100.0;
+        System.out.println("총 수익률은 " + roundProfitRate + "%입니다.");
+    }
+
+    private static Map<LottoRanking, Integer> initializeResultsMap() {
+        return Arrays.stream(LottoRanking.values())
+                .filter(lottoRanking
+                        -> !lottoRanking.equals(LottoRanking.NONE))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toMap(
+                        Function.identity()
+                        , count -> 0
+                        , (count1, count2) -> count1
+                        , LinkedHashMap::new));
+    }
+
+    private static void putResults(
+            List<LottoRanking> results,
+            Map<LottoRanking, Integer> resultsMap
+    ) {
+        results.stream().filter(lottoRanking
+                        -> !lottoRanking.equals(LottoRanking.NONE))
+                .forEach(lottoRanking -> {
+                    resultsMap.put(lottoRanking, resultsMap.get(lottoRanking) + 1);
+                });
+    }
+}
