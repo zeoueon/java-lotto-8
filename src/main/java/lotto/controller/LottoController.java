d4d6a78fafe32ac6fec107759efbbf678445ed2a
zeoueon <zeoueon129@gmail.com>
2025-11-02 22:10:07 +0900

    fix(LottoController): 예외 발생 시 다시 입력 받는 요구 사항 반영 및 수정
    잘못된 입력을 받을 때, 다시 입력을 받도록 수정함.
    run() 함수의 가독성을 위해 startLottoGame 함수를 정의해서 역할을 분리함.

--- src/main/java/lotto/controller/LottoController.java
+++ src/main/java/lotto/controller/LottoController.java
@@ -1,6 +1,7 @@
 package lotto.controller;
 
 import java.util.List;
+import java.util.function.Supplier;
 import lotto.converter.InputParser;
 import lotto.service.LottoGame;
 import lotto.view.InputView;
@@ -16,24 +17,39 @@ public class LottoController {
     }
 
     public void run() {
-        lottoGame = new LottoGame(inputParser
-                .parsePurchaseAmount(InputView.inputPurchaseAmount()));
-
-        OutputView.printLottos(lottoGame.startLottoGame());
-
+        startLottoGame();
         setWinningLotto();
         printResults();
     }
 
+    private void startLottoGame() {
+        int purchaseAmount = retryInput(() ->
+                inputParser.parsePurchaseAmount(InputView.inputPurchaseAmount()));
+        lottoGame = new LottoGame(purchaseAmount);
+        OutputView.printLottos(lottoGame.startLottoGame());
+    }
+
     private void setWinningLotto() {
-        List<Integer> winningLotto = inputParser
-                .parseWinningLotto(InputView.inputWinningNumber());
-        int bonusNumber = inputParser.parseBonusNumber(winningLotto, InputView.inputBonusNumber());
+        List<Integer> winningLotto = retryInput(() ->
+                inputParser.parseWinningLotto(InputView.inputWinningNumber()));
+        int bonusNumber = retryInput(() ->
+                inputParser.parseBonusNumber(winningLotto, InputView.inputBonusNumber()));
+
         lottoGame.setWinningLotto(winningLotto, bonusNumber);
     }
 
     private void printResults() {
         OutputView.printResults(lottoGame.getGameResults());
         OutputView.printProfitRate(lottoGame.calculateProfitRate());
+    }
+
+    private <T> T retryInput(Supplier<T> inputFunction) {
+        while (true) {
+            try {
+                return inputFunction.get();
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
